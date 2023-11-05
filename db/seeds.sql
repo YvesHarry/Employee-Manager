da3863eac2bd353a9f54c1eafb6f87616eae7bab
YvesHarry <yhdeslouches@hotmail.com>
2023-11-05 16:17:04 -0500

    Uploaded all files

---
+++ db/seeds.sql
@@ -0,0 +1,38 @@
+INSERT INTO departments (department_name)
+VALUES 
+('Executive Board'),
+('Marketing'),
+('Human Resources'),
+('Finance'),
+('Engineering'),
+('Information Technology'),
+('Customer Relations'),
+('Research and Development'),
+('Legal'),
+('Maintenance');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+('Chief Executive Officer', 555000.00, 1),
+('Marketing Manager', 125000.00, 2),
+('HR Director', 189000.00, 3),
+('Finance Head', 145000.00, 4),
+('Senior Engineer', 185000.00, 5),
+('IT Manager', 125000.00, 6),
+('Customer Relations Manager', 75000.00, 7),
+('Research and Development Manager ', 185000.00, 8),
+('Legal Manager', 95000.00, 9),
+('Maintenance Manager', 135000.00, 10);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Tim', 'Li', 1, 1),
+('Harry', 'The Developer', 2, 2),
+('Jacob', 'Casady', 3, 3),
+('Saint', 'Don', 4, 4),
+('Paul', 'Jobs', 5, 5),
+('Jack', 'Hustach', 6, 6),
+('Harold', 'Rockland', 7, 7),
+('Annie', 'Ferrari', 8, 8),
+('Laya', 'The disciple', 9, 9),
+('Siru', 'Kasava', 10, 10);
